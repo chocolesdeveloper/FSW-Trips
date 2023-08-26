1f9510611639e8712ebf6239ffacec5ffbe15a40
William <williamferreira.programador@gmail.com>
2023-08-26 11:21:32 -0300

    feat: add route in quick search

--- src/app/components/QuickSearch.tsx
+++ src/app/components/QuickSearch.tsx
@@ -1,4 +1,5 @@
 import Image from "next/image"
+import Link from "next/link"
 
 export function QuickSearch() {
   return (
@@ -13,22 +14,38 @@ export function QuickSearch() {
 
       <div className="flex items-center mt-5">
         <div className="flex w-full justify-around">
-          <button className="flex flex-col items-center gap-1">
-            <Image src="/hotel-icon.png" alt="Hotel" width={25} height={25} />
-            <p className="text-grayPrimary text-sm">Hotel</p>
-          </button>
-          <button className="flex flex-col items-center gap-1">
-            <Image src="/farm-icon.png" alt="Hotel" width={25} height={25} />
-            <p className="text-grayPrimary text-sm">Fazenda</p>
-          </button>
-          <button className="flex flex-col items-center gap-1">
-            <Image src="/cottage-icon.png" alt="Hotel" width={25} height={25} />
-            <p className="text-grayPrimary text-sm">Chalé</p>
-          </button>
-          <button className="flex flex-col items-center gap-1">
-            <Image src="/inn-icon.png" alt="Hotel" width={25} height={25} />
-            <p className="text-grayPrimary text-sm">Pousada</p>
-          </button>
+          <div className="flex flex-col items-center gap-1">
+            <Link href={`/trips/search?text=hotel`}>
+              <div className="flex flex-col items-center">
+                <Image src="/hotel-icon.png" alt="Hotel" width={25} height={25} />
+                <p className="text-grayPrimary text-sm">Hotel</p>
+              </div>
+            </Link>
+          </div>
+          <div className="flex flex-col items-center gap-1">
+            <Link href={`/trips/search?text=fazenda`}>
+              <div className="flex flex-col items-center">
+                <Image src="/farm-icon.png" alt="Fazenda" width={25} height={25} />
+                <p className="text-grayPrimary text-sm">Fazenda</p>
+              </div>
+            </Link>
+          </div>
+          <div className="flex flex-col items-center gap-1">
+            <Link href={`/trips/search?text=chalé`}>
+              <div className="flex flex-col items-center">
+                <Image src="/cottage-icon.png" alt="Chale" width={25} height={25} />
+                <p className="text-grayPrimary text-sm">Chalé</p>
+              </div>
+            </Link>
+          </div>
+          <div className="flex flex-col items-center gap-1">
+            <Link href={`/trips/search?text=pousada`}>
+              <div className="flex flex-col items-center">
+                <Image src="/inn-icon.png" alt="Pousada" width={25} height={25} />
+                <p className="text-grayPrimary text-sm">Pousada</p>
+              </div>
+            </Link>
+          </div>
         </div>
       </div>
     </div>
